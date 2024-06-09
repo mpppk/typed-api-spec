04a06291c2a13a896dd5bdafcc77916ad0bd60fe
mpppk <niboshiporipori@gmail.com>
2024-06-09 14:25:41 +0900

    Organize type test

--- src/common/type.t-test.ts
+++ src/common/type.t-test.ts
@@ -1,8 +1,8 @@
 import { Equal, Expect } from "./type-test";
-import { ExtractByPrefix, Split } from "./type";
+import { ExtractByPrefix, Replace, Split } from "./type";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-type cases = [
+type SplitTestCases = [
   Expect<Equal<Split<"", "">, []>>,
   Expect<Equal<Split<"a", "">, ["a"]>>,
   Expect<Equal<Split<"ab", "">, ["a", "b"]>>,
@@ -10,7 +10,20 @@ type cases = [
   Expect<Equal<Split<"/a/b", "/">, ["", "a", "b"]>>,
   Expect<Equal<Split<"a/b/c", "/">, ["a", "b", "c"]>>,
   Expect<Equal<Split<"a/b/", "/">, ["a", "b", ""]>>,
+];
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type ReplaceTestCases = [
+  Expect<Equal<Replace<"a", "a", "-">, "-">>,
+  Expect<Equal<Replace<"a", "noexist", "-">, "a">>,
+  Expect<Equal<Replace<"a", "a", "a">, "a">>,
+  Expect<Equal<Replace<"abcd", "ab", "-">, "-cd">>,
+  Expect<Equal<Replace<"abcd", "cd", "-">, "ab-">>,
+  Expect<Equal<Replace<"abcd", "bc", "-">, "a-d">>,
+];
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type ExtractByPrefixTestCases = [
   Expect<Equal<ExtractByPrefix<"", "">, "">>,
   Expect<Equal<ExtractByPrefix<"a", "">, "a">>,
   Expect<Equal<ExtractByPrefix<"a" | "b", "">, "a" | "b">>,
--- src/common/url.t-test.ts
+++ src/common/url.t-test.ts
@@ -10,7 +10,7 @@ import {
 } from "./url";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-type ParseUrlParamsCases = [
+type ParseUrlParamsTestCases = [
   // @ts-expect-error undefined is not a string
   Expect<Equal<ParseUrlParams<undefined>, never>>,
   Expect<Equal<ParseUrlParams<"">, never>>,
@@ -19,28 +19,6 @@ type ParseUrlParamsCases = [
   Expect<Equal<ParseUrlParams<"/:a">, "a">>,
   Expect<Equal<ParseUrlParams<"/:a/:b">, "a" | "b">>,
   Expect<Equal<ParseUrlParams<"/a/:b">, "b">>,
-
-  Expect<Equal<ToUrlParamPattern<"">, "">>,
-  Expect<Equal<ToUrlParamPattern<"/">, "/">>,
-  Expect<Equal<ToUrlParamPattern<":a">, string>>,
-  Expect<Equal<ToUrlParamPattern<"/:a/b">, `/${string}/b`>>,
-  Expect<Equal<ToUrlParamPattern<"/:a/:b">, `/${string}/${string}`>>,
-  Expect<
-    // @ts-expect-error URL is not supported
-    Equal<ToUrlParamPattern<"https://example.com">, `"https://example.com}`>
-  >,
-
-  Expect<Equal<ToUrlPattern<"">, "">>,
-  Expect<Equal<ToUrlPattern<"/">, "/">>,
-  Expect<Equal<ToUrlPattern<"/users/:userId">, `/users/${string}`>>,
-  Expect<
-    Equal<
-      ToUrlPattern<"/users/:userId?key=value">,
-      `/users/${string}?key=value`
-    >
-  >,
-  // @ts-expect-error URL is not supported
-  Expect<Equal<ToUrlPattern<"https://example.com">, "https://example.com">>,
 
   Expect<Equal<MatchedPatterns<"", "">, "">>,
   Expect<Equal<MatchedPatterns<"/1", "/:userId">, "/:userId">>,
@@ -62,49 +40,46 @@ type ParseUrlParamsCases = [
 
   Expect<
     Equal<
-      ParseOrigin<"">,
-      {
-        schema: undefined;
-        host: undefined;
-        port: undefined;
-        path: "";
-      }
-    >
-  >,
-
-  Expect<
-    Equal<
       ParseHostAndPort<"example.com">,
-      {
-        host: "example.com";
-        port: undefined;
-      }
+      { host: "example.com"; port: undefined }
     >
   >,
   Expect<
     Equal<
       ParseHostAndPort<"example.com:8080">,
-      {
-        host: "example.com";
-        port: "8080";
-      }
+      { host: "example.com"; port: "8080" }
     >
   >,
   // If invalid port is specified, it should return never
   Expect<Equal<ParseHostAndPort<"example.com:xxx">, never>>,
+];
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type ToUrlParamPatternTestCases = [
+  Expect<Equal<ToUrlParamPattern<"">, "">>,
+  Expect<Equal<ToUrlParamPattern<"/">, "/">>,
+  Expect<Equal<ToUrlParamPattern<":a">, string>>,
+  Expect<Equal<ToUrlParamPattern<"/:a/b">, `/${string}/b`>>,
+  Expect<Equal<ToUrlParamPattern<"/:a/:b">, `/${string}/${string}`>>,
+  Expect<
+    // @ts-expect-error URL is not supported
+    Equal<ToUrlParamPattern<"https://example.com">, `"https://example.com}`>
+  >,
+];
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type ToUrlPatternTestCases = [
+  Expect<Equal<ToUrlPattern<"">, "">>,
+  Expect<Equal<ToUrlPattern<"/">, "/">>,
+  Expect<Equal<ToUrlPattern<"/users/:userId">, `/users/${string}`>>,
   Expect<
     Equal<
-      ParseOrigin<"https://example.com/users/:userId">,
-      {
-        host: "example.com";
-        port: undefined;
-      } & {
-        schema: "https";
-        path: "/users/:userId";
-      }
+      ToUrlPattern<"/users/:userId?key=value">,
+      `/users/${string}?key=value`
     >
   >,
+  // @ts-expect-error URL is not supported
+  Expect<Equal<ToUrlPattern<"https://example.com">, "https://example.com">>,
 ];
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -114,12 +89,6 @@ type ParseOriginCases = [
     Equal<
       ParseOrigin<"">,
       { schema: undefined; host: undefined; port: undefined; path: "" }
-    >
-  >,
-  Expect<
-    Equal<
-      ParseOrigin<"https://example.com">,
-      { host: "example.com"; port: undefined } & { schema: "https"; path: "" }
     >
   >,
   Expect<
@@ -137,19 +106,34 @@ type ParseOriginCases = [
       }
     >
   >,
+
+  Expect<
+    Equal<
+      ParseOrigin<"https://example.com/users/:userId">,
+      { host: "example.com"; port: undefined } & {
+        schema: "https";
+        path: "/users/:userId";
+      }
+    >
+  >,
+
   Expect<
     Equal<
       ParseOrigin<"https://example.com:8080/user">,
       { host: "example.com"; port: "8080" } & { schema: "https"; path: "/user" }
     >
   >,
+
   Expect<
     Equal<
       ParseOrigin<"/user">,
       { schema: undefined; host: undefined; port: undefined; path: "/user" }
     >
   >,
+];
 
-  Expect<Equal<ParseURL<"/user">["path"], "/user">>,
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type ParseURLTestCases = [
+  Expect<Equal<ParseURL<"/user?a=b">["path"], "/user">>,
   Expect<Equal<ParseURL<"https://example.com/user">["path"], "/user">>,
 ];
