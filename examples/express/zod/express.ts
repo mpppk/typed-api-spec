000fb818dbcc17957762d32cdf77860692be8217
mpppk <niboshiporipori@gmail.com>
2024-08-31 22:44:23 +0900

    organize

--- examples/express/zod/express.ts
+++ examples/express/zod/express.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { asAsync } from "../../src/express";
-import { pathMap } from "./spec-zod";
-import { ToHandlers, typed } from "../../src/express/zod";
+import { asAsync } from "../../../src/express";
+import { pathMap } from "./spec";
+import { ToHandlers, typed } from "../../../src/express/zod";
 
 const emptyMiddleware = (
   req: express.Request,
