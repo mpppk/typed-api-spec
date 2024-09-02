7cc38ede637294488458b3afbcf0df298925bda2
mpppk <niboshiporipori@gmail.com>
2024-09-02 18:48:51 +0900

    Add fastify example

--- examples/fastify/zod/fastify.ts
+++ examples/fastify/zod/fastify.ts
@@ -1,5 +1,5 @@
 // Import the framework and instantiate it
-import Fastify from "fastify";
+import Fastify, { FastifyError } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
@@ -7,30 +7,72 @@ import {
 } from "fastify-type-provider-zod";
 import { pathMap } from "../../express/zod/spec";
 import { toRoutes } from "../../../src/fastify/zod";
-const fastify = Fastify({
-  logger: true,
-});
+import { ZodError } from "zod";
+const fastify = Fastify({ logger: true });
 
 fastify.setValidatorCompiler(validatorCompiler);
 fastify.setSerializerCompiler(serializerCompiler);
 const server = fastify.withTypeProvider<ZodTypeProvider>();
-
+server.setErrorHandler<ZodError | FastifyError>((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .type("application/json")
+      .send({ errorMessage: JSON.stringify(error.issues) });
+  }
+  return reply
+    .status(error.statusCode ?? 500)
+    .type("application/json")
+    .send({ errorMessage: error.message });
+});
 const routes = toRoutes(pathMap);
-server.get(
-  "/users",
-  {
-    schema: routes["/users"]["get"]["schema"],
-  },
-  async (request) => {
-    return { userNames: [`page${request.query.page}#user1`] };
-  },
-);
 
+// Define by using route object
 server.route({
   ...routes["/users"]["get"],
-  handler: async (request) => {
+  handler: async (request, reply) => {
     const page = request.query.page;
+    if (Number.isNaN(Number(page))) {
+      return reply.status(400).send({ errorMessage: "page is not a number" });
+    }
     return { userNames: [`page${page}#user1`] };
+  },
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _noExecution = () => {
+  // Or you can also define by using RouteShorthandMethod
+  server.get(
+    "/users",
+    {
+      schema: routes["/users"]["get"]["schema"],
+    },
+    async (request) => {
+      {
+        // @ts-expect-error noexist is not defined in pathMap["/users"]["get"]
+        request.query.noexist;
+      }
+      const page = request.query.page;
+      return { userNames: [`page${page}#user1`] };
+    },
+  );
+};
+
+server.route({
+  ...routes["/users"]["post"],
+  handler: async (request, reply) => {
+    const userName = request.body.userName;
+    return reply
+      .header("Content-Type", "application/json")
+      .send({ userId: userName + "#0" });
+  },
+});
+
+server.route({
+  ...routes["/users/:userId"]["get"],
+  handler: async (request) => {
+    const userId = request.params.userId;
+    return { userName: `user#${userId}` };
   },
 });
 
