7217e165897616397efe6e0431b9e30a94dc6447
mpppk <niboshiporipori@gmail.com>
2024-12-11 15:15:29 +0000

    Fix

--- pkgs/typed-api-spec/tsup.config.ts
+++ pkgs/typed-api-spec/tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  entry: ["src", "!src/**/*.test.ts", "!src/**/*.t-test.ts"],
   target: "es2020",
   format: ["cjs", "esm"],
   clean: true,
